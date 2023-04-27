cf2c77412fecadb8c88043077b1a08cfb8a5f56e
Artur <arturremizov@gmail.com>
2023-04-27 23:41:27 +0300

    add LocalNotificationsWithAsyncAwait Initial App

---
+++ LocalNotificationsWithAsyncAwait/LocalNotificationsWithAsyncAwait/LocalNotificationsWithAsyncAwaitApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalNotificationsWithAsyncAwaitApp.swift
+//  LocalNotificationsWithAsyncAwait
+//
+//  Created by Artur Remizov on 27.04.23.
+//
+
+import SwiftUI
+
+@main
+struct LocalNotificationsWithAsyncAwaitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
